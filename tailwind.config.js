f8781c7ef48526156d889f3a288af50d540c5021
RoTour <rothewawa@gmail.com>
2022-03-15 12:02:12 +0100

    update: cards with BG

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     f("src/assets/*")
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ecogreen": '#28592B'
+      },
+      animation: {
+        flip: 'flip 500ms ease-in-out infinite'
+      },
+      keyframes: {
+        flip: {
+          '0%': { transform: 'rotateY(0)' },
+          '50%': { background: "url('assets/feuilles-vertes-carres-06.png')" },
+          '100%': { transform: 'rotateY(180deg)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
