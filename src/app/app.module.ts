327c84655b9b4ee828f08399ecb1128b700916eb
kopenkinda <kopenkin.da@gmail.com>
2022-03-15 12:00:38 +0100

    feat(#12): initialized component + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,9 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GameComponent } from './game/game.component';
-
-const routes: Routes = [{ path: '', component: HomeComponent }];
-
 @NgModule({
-  declarations: [AppComponent, GameComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent, HomeComponent, GameComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
