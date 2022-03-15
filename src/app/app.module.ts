de5d3b3cccf11806947cef37916c42399e185c61
kopenkinda <kopenkin.da@gmail.com>
2022-03-15 11:21:18 +0100

    feat(#16): Imported questions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {RouterModule, Routes} from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
 
-const routes: Routes = [
-  {path: '', component: HomeComponent},
-]
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(routes)
-  ],
+  declarations: [AppComponent, GameComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  content: ["./src/**/*.{html,ts}", "./projects/**/*.{html,ts}"],
   theme: {
     maxWidth: {
-      '1/4': '25%',
+      "1/4": "25%",
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#28592B",
+      },
+    },
   },
   plugins: [],
- };
+};
