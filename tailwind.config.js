d9776157457993ad1b89fb01685728bc5f869748
Rémi Falcati <remi.falcati@ynov.com>
2022-03-15 08:47:50 +0100

    Header

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  theme: {
+    maxWidth: {
+      '1/4': '25%',
+    },
+    extend: {},
+  },
+  plugins: [],
+ };
