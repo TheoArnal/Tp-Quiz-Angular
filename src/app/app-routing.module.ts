534ef7a63274ac59b79da76db37730c5f7d03bae
rotour <rothewawa@gmail.com>
2022-03-20 11:15:02 +0100

    upgrade: navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { HomeComponent } from './home/home.component';
+import { TipsComponent } from "./tips/tips.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  {
-    path: 'game',
-    component: GameComponent,
-  },
+  { path: 'game', component: GameComponent },
+  { path: 'tips', component: TipsComponent },
 ];
 
 @NgModule({
