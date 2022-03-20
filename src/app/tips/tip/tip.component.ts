e2676a2e50cd9479184d5b5c5867cfa3a580ac5c
rotour <rothewawa@gmail.com>
2022-03-20 16:41:29 +0100

    add: tips list

--- src/app/tips/tip/tip.component.ts
+++ src/app/tips/tip/tip.component.ts
@@ -23,12 +23,23 @@ export class TipComponent implements OnInit, OnChanges {
   ]
 
   tips = [
-    "Ne mets pas ton doigt dans la friteuse",
-    "Fais attention avec une agrafeuse, tu risques de te pincer très très très très très très très très très très très très très très fort",
-    '3',
-    '4',
-    '5',
-    '6',
+    "Pour économiser de l'eau aux toilettes, je deplace le flotteur de la chasse pour réduire le volume d'eau, ou j'utilise des bouteilles d'eau pour la petite commission.",
+    "Pour mes déplacements, j'essaie le vélo ou la marche au lieu de la voiture lorsque le temps le permet, sinon je préfère les transport en commun.\n",
+    "Je remplace l'eau de Javel, toxique, par du vinaigre blanc: il ne pollue pas les eaux et est très économique.\n",
+    "Ma plaquette de médicament n'est pas terminé mais est pérnimée depuis longtemps ? Je la rapport à la pharmacie.\n",
+    "Quand c'est possible pour les longs trajets en France, je privilegie le train à l'avion: j'arriverai souvent plus vite et la planète me dira merci.\n",
+    "Chauffer son intérieur à 19°C et le baisser lorsque vous vous absentez.\n",
+    "Pour laver votre sol, vous pouvez utiliser le savon noir : une cuillère à soupe de savon noir par litre d’eau chaude (eau tiède pour tomettes ou parquet !).\n",
+    "Utiliser des lampes basse consommation (remplacez les halogènes par des LED).\n",
+    "Privilégier les programmes « ECO » si vous utilisez un lave-vaisselle/lave-linge.\n",
+    "Privilégier les douches aux bains.\n",
+    "Faire pipi dans la douche c’est écolo et ça évite une chasse d’eau tous les matins ! (et acheter du papier toilette recyclé et recyclable labellisé par l’écolabel européen)\n",
+    "On privilégie l’achat de matières plus écologiques comme le bois, le verre ou l’inox.\n",
+    "Pour faire des économies d’énergie, privilégiez le matériel informatique et bureautique avec le label Energy Star.\n",
+    "Optimiser ses recherches sur internet, en enregistrant ses sites souvent consultés en favoris.\n",
+    "Et utiliser un moteur de recherche écologique qui aide à planter des arbres grâce à Ecosia.\n",
+    "Votre boite e-mail ne doit pas être trop surchargée. Faites un tri régulièrement, desinscrivez vous de certains abonnements qui ne vous intéressent plus.\n",
+    "Optimisez la taille des documents que vous envoyez par mail pour qu’ils soient le plus léger possible.",
   ]
 
   currentBg!: string
--- src/app/tips/tips.component.ts
+++ src/app/tips/tips.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { getRandomArrayElement } from "../../utils/array-utils";
 import { Subject } from "rxjs";
 
 @Component({
@@ -12,40 +11,14 @@ export class TipsComponent implements OnInit {
   constructor() { }
 
   generated: string[] = [];
-  currentCardIndex = 0;
-
-  tips = [
-    "Ne mets pas ton doigt dans la friteuse",
-    "Fais attention avec une agrafeuse, tu risques de te pincer très très très très très très très très très très très très très très fort"
-  ]
-
-  // backgrounds = [
-  //   "feuilles-dores-carre.png",
-  //   "feuilles-orange.png",
-  //   "feuilles-vertes-carres-06.png",
-  // ]
 
   loadNewTip = new Subject<void>()
 
   ngOnInit(): void {
-    this.generated.push(getRandomArrayElement(this.tips))
   }
 
 
-  moveCursor(direction: 'forward' | 'backward') {
+  moveCursor() {
     this.loadNewTip.next()
-    // this.getRandomBg()
-    this.currentCardIndex += direction === 'forward' ? 1 : -1;
-    if (this.currentCardIndex === this.generated.length) {
-      this.generated.push(getRandomArrayElement(
-        this.tips.filter((it) => it !== this.generated[this.currentCardIndex-1])
-      ));
-      console.log(`current: ${this.generated[this.currentCardIndex]}, last: ${this.generated[this.generated.length-1]}`);
-      return;
-    }
-    if (this.currentCardIndex === -1) {
-      this.generated = [getRandomArrayElement(this.tips), ...this.generated];
-      this.currentCardIndex = 0;
-    }
   }
 }
